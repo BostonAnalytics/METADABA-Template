cc9eeca2d723ab51508e584800654e7f20aac148
Nakul Ravindra Padalkar <2807210+nakulpadalkar@users.noreply.github.com>
2024-12-09 16:40:19 -0500

    removed company description

--- data/linkedin_20_3NF.sql
+++ data/linkedin_20_3NF.sql
@@ -39,7 +39,7 @@ CREATE TABLE companies (
 ) ;
 
 
-INSERT INTO companies(company_id,company_name,company_size,state,country,city,zip_code,company_address,company_url) VALUES (1009, "IBM",7,"NY","US","Armonk, New York","10504","International Business Machines Corp.","https://www.linkedin.com/company/ibm\r");
+INSERT INTO companies(company_id,company_name,company_size,state,country,city,zip_code,company_address,company_url) VALUES (1009, 'IBM',7,'NY','US','Armonk, New York','10504','International Business Machines Corp.','https://www.linkedin.com/company/ibm\r');
 
 -- Table structure for table `company_industries`
 
